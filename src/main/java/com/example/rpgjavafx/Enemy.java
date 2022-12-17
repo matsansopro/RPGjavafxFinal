158d604ab734273fd2008f1bb7e6a9b517815aa9
matsansopro <116085065+matsansopro@users.noreply.github.com>
2022-12-17 19:54:02 +0100

    Update Enemy.java

--- src/main/java/com/example/rpgjavafx/Enemy.java
+++ src/main/java/com/example/rpgjavafx/Enemy.java
@@ -5,16 +5,19 @@ class Enemy extends Combattant {
         super(60, 50, 20);
 
     }
-    public void attack(Hero hero){
+
+    public void attack(Hero hero) {
         hero.takeDamage(this.ATK);
 
     }
-    public void takeDamage(int damage){
-        this.HP-= damage;
+
+    public void takeDamage(int damage) {
+        this.HP -= damage;
     }
-    public String getStats(){
-        return this.name + this.HP + this.ATK + this.defense;
+
+    public String getStats() {
+        String stats = this.name + Integer.toString(this.HP) + Integer.toString(this.ATK) + Integer.toString(this.defense);
+        return stats;
     }
 }
-
 
